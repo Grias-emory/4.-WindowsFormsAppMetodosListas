33af5173667d029a821aa2e346fe0f2fc527044f
Grias-emory <benjalejandroraza@gmail.com>
2025-05-04 12:46:02 -0500

    Primer Commit

---
+++ WindowsFormsAppMetodosListas/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsAppMetodosListas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lstElementos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(28, 49);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(100, 22);
+            this.txtElemento.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(40, 103);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 35);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(40, 161);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 34);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lstElementos
+            // 
+            this.lstElementos.FormattingEnabled = true;
+            this.lstElementos.ItemHeight = 16;
+            this.lstElementos.Location = new System.Drawing.Point(153, 49);
+            this.lstElementos.Name = "lstElementos";
+            this.lstElementos.Size = new System.Drawing.Size(176, 164);
+            this.lstElementos.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstElementos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtElemento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.ListBox lstElementos;
+    }
+}
+
---
+++ WindowsFormsAppMetodosListas/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppMetodosListas
+{
+    public partial class Form1: Form
+    {
+        // Creamos una lista de cadenas para almacenar los elementos que el usuario agregue
+        private List<string> elementos = new List<string>();
+
+        public Form1()
+        {
+            // Inicializa todos los componentes visuales del formulario (botones, cajas de texto, etc.)
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Obtenemos el texto ingresado por el usuario
+            string nuevoElemento = txtElemento.Text;
+
+            // Validamos que el texto no esté vacío ni solo con espacios
+            if (!string.IsNullOrWhiteSpace(nuevoElemento))
+            {
+                // Agregamos el nuevo elemento a la lista
+                elementos.Add(nuevoElemento);
+
+                // Actualizamos la visualización de la lista en el ListBox
+                ActualizarLista();
+
+                // Limpiamos la caja de texto para ingresar un nuevo elemento
+                txtElemento.Clear();
+            }
+            else
+            {
+                // Mostramos un mensaje si el texto ingresado no es válido
+                MessageBox.Show("Por favor ingresa un elemento válido.");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Verificamos si hay un elemento seleccionado en el ListBox
+            if (lstElementos.SelectedItem != null)
+            {
+                // Eliminamos el elemento seleccionado de la lista
+                elementos.Remove(lstElementos.SelectedItem.ToString());
+                // Actualizamos la visualización del ListBox
+                ActualizarLista();
+            }
+            else
+            {
+                // Mostramos un mensaje si no hay ningún elemento seleccionado
+                MessageBox.Show("Selecciona un elemento para eliminar.");
+            }
+        }
+        // Método para actualizar ListBox 
+
+        private void ActualizarLista()
+        {
+            // Asignamos null temporalmente para forzar la actualización del contenido
+            lstElementos.DataSource = null;
+
+            // Asignamos nuevamente la lista actualizada como fuente de datos del ListBox
+            lstElementos.DataSource = elementos;
+        }
+    }
+}
